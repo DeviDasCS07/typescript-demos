72cbbccb10125667891bdb93df71e8c14e050e4b
Devi <devidas.cse07@gmail.com>
2024-04-28 12:45:45 +0530

    initial commit

---
+++ type-narrowing.ts
@@ -0,0 +1,15 @@
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+type Human = { swim?: () => void; fly?: () => void };
+
+function move(animal: Fish | Bird | Human) {
+    if ("swim" in animal) {
+        animal;
+
+        //(parameter) animal: Fish | Human
+    } else {
+        animal;
+
+        //(parameter) animal: Bird | Human
+    }
+}
